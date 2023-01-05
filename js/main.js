e348ae17aba71cd6f20253e7476ba5e8f53348be
npxrus <npxrus@yandex.com>
2023-01-05 14:24:22 +0300

    feat: Добавляет функцию получений случайного числа с плавающей точкой из диапазона

--- js/main.js
+++ js/main.js
@@ -17,4 +17,23 @@ function getRandomIntegerNumber(from, to) {
   return Math.floor(Math.random() * (to - from + 1)) + from;
 }
 
+function getRandomFloatNumber(from, to, digits) {
+  if (from < 0 || to < 0) {
+    return undefined;
+  }
+
+  if (from > to) {
+    [from, to] = [to, from];
+  }
+
+  if (from === to) {
+    return from;
+  }
+
+  const result = Math.random() * (to - from) + from;
+
+  return result.toFixed(digits);
+}
+
 getRandomIntegerNumber(1, 10);
+getRandomFloatNumber(0.1, 0.4, 3);
