012a23dedf7cd53d5f6b92449fa7f5b666aad933
npxrus <npxrus@yandex.com>
2023-01-05 14:14:07 +0300

    feat: Добавляет функцию получения случайного целого числа из диапазона

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function getRandomIntegerNumber(from, to) {
+  if (from < 0 || to < 0) {
+    return undefined;
+  }
+
+  if (from > to) {
+    [from, to] = [to, from];
+  }
+
+  if (from === to) {
+    return from;
+  }
+
+  from = Math.ceil(from);
+  to = Math.ceil(to);
+
+  return Math.floor(Math.random() * (to - from + 1)) + from;
+}
+
+getRandomIntegerNumber(1, 10);
